1abfd26f6b8d71df34a0944887c759045908969f
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-04-24 18:58:55 -0400

    added html form, server

--- calculator.js
+++ calculator.js
@@ -1,6 +1,15 @@
 //
 //VARIABLES
 //
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
 
 const readline = require("readline").createInterface({
   input: process.stdin,
