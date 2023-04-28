208c54747c03b3595d5a348ba9d156d5b41a74cd
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-04-28 11:24:09 -0400

    added write to dynamo option

--- public/calculator.js
+++ public/calculator.js
@@ -2,7 +2,6 @@
 //VARIABLES
 //
 
-
 var Answer = "";
 var valuesForOperation = [];
 
@@ -41,14 +40,22 @@ var operations = [
 //FUNCTIONS
 //
 
-
 function performOperation(operation, userInput) {
-  let operationObj = operations.find(x => x.operation === operation);
-        valuesForOperation = mapUserInputToCalculableValues(userInput);
-        this.Answer = operationObj.logic(valuesForOperation);
-        console.log(`result: ${this.Answer}`);
-        let displayResult = document.getElementById('result');
-        displayResult.value = `result: ${this.Answer}`;
+  if (operation === "Clear Result") {
+    this.Answer = "";
+    setDisplayValue();
+    return;
+  }
+
+  if (containsOnlyNumbers(userInput)) {
+    let operationObj = operations.find((x) => x.operation === operation);
+    valuesForOperation = mapUserInputToCalculableValues(userInput);
+    this.Answer = operationObj.logic(valuesForOperation);
+    setDisplayValue();
+  } else {
+    alert("Please only enter numbers or spaces");
+    return;
+  }
 }
 
 function mapUserInputToCalculableValues(userInput) {
@@ -81,5 +88,78 @@ function convertArrayElementsToNumbers(cleanStringArray) {
   return cleanStringArray.map(Number);
 }
 
+function setDisplayValue() {
+  const showResultElement = document.getElementById("showResult");
+  showResultElement.innerText = `result: ${this.Answer}`;
+}
 
+function containsOnlyNumbers(str) {
+  return /^(?=.*\d)[\d ]+$/.test(str);
+}
 
+function clearInputField() {
+  const showResultElement = document.getElementById("userInput");
+  showResultElement.value = null;
+}
+
+async function writeToDynamoDB() {
+  let timestamp = getTimeStamp();
+  let reqObj = `{\"timestamp\": \"${timestamp}\", \"result\": \"${this.Answer}\"}`;
+  await fetch("https://fom8wr9ivi.execute-api.us-east-1.amazonaws.com/items", {
+    method: "put",
+    body: reqObj,
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+    })
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
+
+function getRecords() {
+  fetch("https://fom8wr9ivi.execute-api.us-east-1.amazonaws.com/items")
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function (dynamoRes) {
+        let table = document.getElementById("previousResultsTable");
+        table.innerHTML = "";
+        dynamoRes.forEach((res) => {
+          let tr = document.createElement("tr");
+          Object.values(res).forEach((value) => {
+            let td = document.createElement("td");
+            td.innerHTML = value;
+            tr.appendChild(td);
+          });
+          table.appendChild(tr);
+        });
+        var header = table.createTHead();
+        var row = header.insertRow(0);
+        var cell = row.insertCell(0);
+        cell.innerHTML = "<b>Timestamp</b>";
+        var cell = row.insertCell(1);
+        cell.innerHTML = "<b>Result</b>";
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
+
+function getTimeStamp() {
+  const currentTime = Date.now();
+  const dateObject = new Date(currentTime);
+  const timestamp = dateObject.toISOString();
+  return timestamp;
+}
--- server.js
+++ server.js
@@ -2,15 +2,10 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
-// Express Middleware for serving static files
 app.use(express.static('public'))
 
 app.get("/", function (req, res) {
-    res.sendFile("C:/Users/MclawhornN/nodeCalculator/index.html");
-});
-
-app.post("/", function (req, res) {
-    console.log(req)
+    res.sendFile(__dirname+"/index.html");
 });
 
 app.listen(3000);
